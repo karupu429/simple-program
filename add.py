f8ad9e2c91964fd46aebda6ea6d47e7425cf2b8d
karupu429 <wwwmganesh429@gamil.com>
2024-02-19 17:08:06 +0000

    feat : add two numbers & min to sec

---
+++ add.py
@@ -0,0 +1,12 @@
+x = 5
+y = 10
+print("THE NUMBERS 5 & 10 ADD =",x + y)
+
+print("---------------------------------------------------------------------")
+ 
+# Python Program to Convert seconds
+# into hours, minutes and seconds
+ 
+Minutes = int(input("Type an amount of time in minutes: "))
+Seconds = Minutes * 60
+print(Minutes, "Minutes =", Seconds, "Seconds")
